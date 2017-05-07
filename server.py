6648a025c11e90386fc88b87355c764889c5ddec
Maxim <endurancemayer@yandex.ru>
2017-05-07 23:38:19 +0500

    starting launch() function and working with client

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 import socket
 import sys
+from cache import Dns_Cache
+from multiprocessing.dummy import Pool as ThreadPool
+from select import select
 
 
 class Dns_Server:
-    def __init__(self, hello_word="Hello"):
+    def __init__(self, hello_word="Hello! Ready for a job"):
         self.welcome = hello_word
         self.cache = None
         self.address = 'localhost'
         self.port = 53
         self.forwarder = None
+        self.pool = None
 
     def set_up_address(self, address='localhost'):
         self.address = address
@@ -33,8 +37,15 @@ class Dns_Server:
             self.forwarder = forwarder
         return self
 
-    def set_up_cache(self, cache):
-        pass
+    def set_up_cache(self, cache=None):
+        if cache is None:
+            self.cache = Dns_Cache(database_name="cache using python dictionary")
+            # one session cache, could be replaced
+        return self
+
+    def apply_async(self, pool=None):
+        if pool is None:
+            self.pool = ThreadPool(processes=4)
         return self
 
     def __check_all_set_up__(self):
@@ -43,11 +54,36 @@ class Dns_Server:
             if v is None:
                 raise Exception("{} is not defined".format(k))
 
-    def launch(self):
-        self.__check_all_set_up__()
+    def __try_bind_connection__(self, connection):
+        try:
+            connection.bind((self.address, self.port))
+        except socket.error:
+            print("Could not bind pair ({}, {})".format(self.address, self.port))
+        else:
+            return True
+
+    @classmethod
+    def client_worker(cls, request):
+        """
+        a function to work with client - form answering packet and send it
+        :param request: string domain name: google.com, www.vk.com or common
+        """
         pass
 
-Dns_Server().__check_all_set_up__()
+    def launch(self):
+        self.__check_all_set_up__()
+        print(self.welcome)
+        connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__try_bind_connection__(connection)
+        while True:
+            reading, _, _ = select([connection], [], [])
+            if reading:
+                try:
+                    bin_data, address = connection.recvfrom(512)
+                except socket.error:
+                    print("Couldn't receive from client")
+
+
 
 # a = Dns_Server("Hello").set_up_address().set_up_port().set_up_forwarder("google.com")
 
